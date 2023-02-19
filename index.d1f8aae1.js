c2b2b6c56d57c113e6de41b350737e73d57d5a37
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-02-19 20:25:18 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/Parallax-waves@181a469f7ef8fad996bdc27fc03d7a52f15cd3a8 🚀

---
+++ index.d1f8aae1.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("wave1"),t=document.getElementById("wave2"),n=document.getElementById("wave3"),o=document.getElementById("wave4");window.addEventListener("scroll",(function(){let d=this.scrollY;e.style.backgroundPositionX=400+4*d+"px",t.style.backgroundPositionX=300+-4*d+"px",n.style.backgroundPositionX=200+2*d+"px",o.style.backgroundPositionX=100+-2*d+"px"}));
+//# sourceMappingURL=index.d1f8aae1.js.map
---
+++ index.e07b754e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("wave1"),t=document.getElementById("wave2"),n=document.getElementById("wave3"),o=document.getElementById("wave4");window.addEventListener("scroll",(function(){var d=this.scrollY;e.style.backgroundPositionX=400+4*d+"px",t.style.backgroundPositionX=300+-4*d+"px",n.style.backgroundPositionX=200+2*d+"px",o.style.backgroundPositionX=100+-2*d+"px"}))}();
+//# sourceMappingURL=index.e07b754e.js.map
